5db2e6e318d4a1b9bdb88e4e1a2652680b3e3e82
Nguyen Van Vu <nguyenvanvu.bkhn@gmail.com>
2017-09-10 02:34:21 +0700

    add-login-mvp

---
+++ app/src/main/java/com/workspace/mvp_login/presenter/LoginPresenterContract.java
@@ -0,0 +1,9 @@
+package com.workspace.mvp_login.presenter;
+
+/**
+ * Created by workspace on 10/09/2017.
+ */
+
+public interface LoginPresenterContract {
+    public void handlingLogin(String userName,String password);
+}
---
+++ app/src/main/java/com/workspace/mvp_login/view/ViewHandlingLogin.java
@@ -0,0 +1,10 @@
+package com.workspace.mvp_login.view;
+
+/**
+ * Created by workspace on 10/09/2017.
+ */
+
+public interface ViewHandlingLogin {
+    public void onSuccess();
+    public void onError();
+}
